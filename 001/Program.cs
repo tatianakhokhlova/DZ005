e1f22647802ba4f40bf3fd29597d64df37f9b345
Tatyana <tatyana.dovganskaya@gmail.com>
2022-10-24 09:26:16 +0300

    1 раздел функций и их реализация, выравнила

--- 001/Program.cs
+++ 001/Program.cs
@@ -1,35 +1,34 @@
 ﻿
-void PrintArray(int[] array) //Печать массива на экран
-{
-    for(int i = 0; i < array.Length; i++)
-    Console.Write($"{array[i]} ");
-Console.WriteLine();
-}
+    void PrintArray(int[] array) //Печать массива на экран
+    {
+        for(int i = 0; i < array.Length; i++)
+        Console.Write($"{array[i]} ");
+    Console.WriteLine();
+    }
 
-void FillArray(int[] array, int min, int max) // Диапазон массива;
-{
-   for(int i = 0; i < array.Length; i++)
-       array[i] = new Random().Next(min,max);  
-}
-int MinArray(int[] array) // Поиск минимума
-{
-int min = array[0];
- for(int i = 0; i<array.Length; i++)
-{
-    if(array[i]<min)
-       min = array[i];
-}
-return min;  
-}
+    void FillArray(int[] array, int min, int max) // Диапазон массива;
+    {
+    for(int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(min,max);  
+    }
+
+    int MinArray(int[] array) // Поиск минимума
+    {
+    int min = array[0];
+    for(int i = 0; i<array.Length; i++)
+    {
+        if(array[i]<min)
+        min = array[i];
+    }
+    return min;  
+    }
 
 int MaxArray(int[] array) // Поиск максимума
 {
     int max = array[0];
-     for(int i = 0; i < array.Length; i++)
-    {
+    for(int i = 0; i < array.Length; i++)
         if(array[i]>max)
            max = array[i];
-    }
 return max;  
 }
 
@@ -50,13 +49,13 @@ return mult;
 }
 
 // Поиск индекса заданного элемента в массиве, если такого элемента в массиве нет то возвращать -1
-int ValueArray(int [] array, int number)
-{
-   for(int i = 0; i < array.Length; i++)
-       if(array[i]==number)
-       return i;    
-return - 1;        
-}
+    int ValueArray(int [] array, int number)
+    {
+    for(int i = 0; i < array.Length; i++)
+        if(array[i]==number)
+        return i;    
+    return - 1;        
+    }
 
 // Среднее арифметическое элеметов массива
 int MidArray(int[] array)
@@ -75,16 +74,12 @@ int NegativCount(int[] array)
 {
 int negativcount = 0;
 for(int i = 0; i < array.Length; i++)
-{
     if(array[i]<0)
        negativcount++;
-}
 return negativcount;
 }
 
 // Подсчёт количества вхождений элемента в массив
-
-
 int Count(int [] array, int value)
 {
     int count = 0;
@@ -99,51 +94,42 @@ int EvenArray(int[] array)
 {
 int count2 = 0;
  for(int i = 0; i < array.Length; i++)
- {
     if(array[i]%2==0)
        count2++;
- }
 return count2; 
 }
 
 // Подсчёт количества положительных элементов в массиве
-
 int PositiveCount(int[] array)
 {
 int positivecount = 0;
 for(int i = 0; i < array.Length; i++)
-{
     if(array[i]>0)
        positivecount++;
-}
 return positivecount;
 }
 
 // Подсчёт количества нечётных элементов в массиве
-
 int UnevenArray(int[] array)
 {
 int count3 = 0;
  for(int i = 0; i < array.Length; i++)
- {
     if(array[i]%2==1)
        count3++;
- }
 return count3;
 }
 
 // Проверка наличия элемента в массиве. Возвращает true, если элемент в массиве есть, false – нет.
-
-bool TrueArray(int [] array, int num)
-{
-   for(int i = 0; i < array.Length; i++)
-       if(array[i]==num)
-       return true;    
-return false;        
-}
-
+    bool TrueArray(int [] array, int num)
+    {
+    for(int i = 0; i < array.Length; i++)
+        if(array[i]==num)
+        return true;    
+    return false;        
+    }
 
 
+// Проверка является ли массив отсортированным по возрастанию. Если массив отсортирован, то возвращать true, иначе - false.
 bool SortArray(int [] array)
  {
     for (int i = 0; i < array.Length - 1; i++)
@@ -156,7 +142,6 @@ bool SortArray(int [] array)
 Console.Write("Введите размер массива: ");
 int length = int.Parse(Console.ReadLine() ?? "0");
 int[] array = new int[length];
-
 FillArray(array, -10,15);
 
 Console.Write("Ваш массив: ");
